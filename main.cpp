0daf95a0f997ef7ef1b13ee1685ef47bf0e13b37
Bayu Muhamad Arib Irfani <2503010123@unper.ac.id>
2026-07-08 09:46:50 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,15 @@
 #include <string>
 using namespace std;
 
-#include "Penumpang.cpp"
 #include "stasiun.cpp"
-#include "manajemenJadwalPerjalanan.cpp"
-#include "perpindahan_jalur.cpp"
 #include "rute.cpp"
+#include "Penumpang.cpp"
+#include "manajementJadwalPerjalanan.cpp"
+#include "perpindahan_jalur.cpp"
 
 int main() {
+    inisialisasiRute();
+
     int pilihan;
     do {
         cout << "\n======================================\n";
@@ -24,18 +26,12 @@ int main() {
         cout << "Pilihan: "; cin >> pilihan;
 
         switch (pilihan) {
-            case 1: menuManajemenStasiun();
-                break;
-            case 2: menuJadwalPerjalanan();
-                break;
-            case 3: menuPerpindahanJalur();
-                break;
-            case 4: menuPencarianRute();
-                break;
-            case 5: menuDataPenumpang();
-                break;
-            case 0: cout << "\nTerima kasih! Sampai jumpa!\n";
-                break;
+            case 1: menuManajemenStasiun(); break;
+            case 2: menuJadwalPerjalanan(); break;
+            case 3: menuPerpindahanJalur(); break;
+            case 4: menuPencarianRute(); break;
+            case 5: menuDataPenumpang(); break;
+            case 0: cout << "\nTerima kasih! Sampai jumpa!\n"; break;
             default: cout << "\nPilihan tidak valid!\n";
         }
     } while (pilihan != 0);
